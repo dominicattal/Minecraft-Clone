a9e8743ba0f6ced279f1292fa65fde0ed8f1885a
da526 <da526@njit.edu>
2023-12-05 14:37:35 -0500

    types

--- src/util/types.h
+++ src/util/types.h
@@ -1,4 +1,17 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+typedef unsigned short     u8;
+typedef unsigned int       u16;
+typedef unsigned long      u32;
+typedef unsigned long long u64;
+
+typedef short     u8;
+typedef int       u16;
+typedef long      u32;
+typedef long long u64;
+
+typedef float f32;
+typedef double f64;
+
 #endif
