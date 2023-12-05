0225550790db21be50ad8f74f7e8d089cabd308e
da526 <da526@njit.edu>
2023-12-05 14:45:07 -0500

    Changed types

--- src/util/types.h
+++ src/util/types.h
@@ -1,20 +1,12 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#include <stdint.h>
-#include <inttypes.h>
+typedef unsigned short u16;
+typedef unsigned int   u32;
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
+typedef short s16;
+typedef int   s32;
 
 typedef float f32;
-typedef double f64;
 
 #endif
